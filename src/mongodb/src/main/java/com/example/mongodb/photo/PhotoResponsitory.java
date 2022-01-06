86028c366fb3726c3a536a5d78d9e48f3e2f1243
wander23 <57674214+wander23@users.noreply.github.com>
2022-01-06 21:01:00 +0700

    merger

--- src/mongodb/src/main/java/com/example/mongodb/photo/PhotoResponsitory.java
+++
@@ -1,9 +0,0 @@
-package com.example.mongodb.photo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import java.util.List;
-
-public interface PhotoResponsitory extends MongoRepository<Photo,String> {
-    List<Photo> findByIdUser(String id);
-}
